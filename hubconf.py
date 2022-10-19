727fcd7f66f8644ca7204da540cdd9297f94db7c
cs19b008iittp2 <116138664+cs19b008iittp2@users.noreply.github.com>
2022-10-19 10:30:38 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -102,3 +102,53 @@ def get_model(train_data_loader=None, n_epochs=10):
     print ('Returning model... (rollnumber: cs19b008)')
   
     return model
+  
+# sample invocation torch.hub.load(myrepo,'get_model_advanced',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
+def get_model_advanced(train_data_loader=None, n_epochs=10,lr=1e-4,config=None):
+    model = None
+
+  # write your code here as per instructions
+  # ... your code ...
+  # ... your code ...
+  # ... and so on ...
+  # Use softmax and cross entropy loss functions
+  # set model variable to proper object, make use of train_data
+  
+  # In addition,
+  # Refer to config dict, where learning rate is given, 
+  # List of (in_channels, out_channels, kernel_size, stride=1, padding='same')  are specified
+  # Example, config = [(1,10,(3,3),1,'same'), (10,3,(5,5),1,'same'), (3,1,(7,7),1,'same')], it can have any number of elements
+  # You need to create 2d convoution layers as per specification above in each element
+  # You need to add a proper fully connected layer as the last layer
+  
+  # HINT: You can print sizes of tensors to get an idea of the size of the fc layer required
+  # HINT: Flatten function can also be used if required
+    return model
+    
+    
+    print ('Returning model... (rollnumber: cs19b008)')
+    
+    return model
+
+    # sample invocation torch.hub.load(myrepo,'test_model',model1=model,test_data_loader=test_data_loader,force_reload=True)
+def test_model(model1=None, test_data_loader=None):
+
+    accuracy_val, precision_val, recall_val, f1score_val = 0, 0, 0, 0
+    # write your code here as per instructions
+    # ... your code ...
+    # ... your code ...
+    # ... and so on ...
+    # calculate accuracy, precision, recall and f1score
+    cnn.eval()
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in loaders['test']:
+            test_output, last_layer = cnn(images)
+            pred_y = torch.max(test_output, 1)[1].data.squeeze()
+            accuracy = (pred_y == labels).sum().item() / float(labels.size(0))
+            pass
+    
+    print ('Returning metrics... (rollnumber: cs19b008)')
+    
+    return accuracy_val, precision_val, recall_val, f1score_val
