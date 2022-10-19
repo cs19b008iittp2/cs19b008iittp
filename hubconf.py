dd4feceeea14e846f67faab08137aec505a862b0
cs19b008iittp2 <116138664+cs19b008iittp2@users.noreply.github.com>
2022-10-19 09:40:58 +0530

    Create hubconf.py

---
+++ hubconf.py
@@ -0,0 +1,9 @@
+dependencies = ['torch']
+
+import torch
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+import torch.nn as nn
+from torch import optim
+from torch.autograd import Variable
