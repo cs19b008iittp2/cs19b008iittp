e2cc6ff1f9f33e26fafd4e14b2a8a16aa8dbed69
cs19b008iittp2 <116138664+cs19b008iittp2@users.noreply.github.com>
2022-10-19 09:54:42 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -9,36 +9,45 @@ from torch import optim
 from torch.autograd import Variable
 
 # Device configuration
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')# Define a neural network YOUR ROLL NUMBER (all small letters) should prefix the classname
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+def kali():
+  print ('kali')
+  
 # Define a neural network YOUR ROLL NUMBER (all small letters) should prefix the classname
-def load_data():
 
-    train_data = datasets.FashionMNIST(
-    root = 'data',
-    train = True,                         
-    transform = ToTensor(), 
-    download = True,            
-    )
-    test_data = datasets.FashionMNIST(
-        root = 'data', 
-        train = False, 
-        transform = ToTensor()
-    )
 
-    return train_data, test_data
-    
-def get_dataloaders(train_data, test_data):
-    loaders = {
-    'train' : torch.utils.data.DataLoader(train_data, 
-                                          batch_size=100, 
-                                          shuffle=True, 
-                                          num_workers=1),
-    
-    'test'  : torch.utils.data.DataLoader(test_data, 
-                                          batch_size=100, 
-                                          shuffle=True, 
-                                          num_workers=1),
-    }
-    return loaders
+
+class cs19b008_CNN(nn.Module):
+    def __init__(self):
+        super(cs19b008_CNN, self).__init__()
+        self.conv1 = nn.Sequential(         
+            nn.Conv2d(
+                in_channels=1,              
+                out_channels=16,            
+                kernel_size=5,              
+                stride=1,                   
+                padding=2,                  
+            ),                              
+            nn.ReLU(),                      
+            nn.MaxPool2d(kernel_size=2),    
+        )
+        self.conv2 = nn.Sequential(         
+            nn.Conv2d(16, 32, 5, 1, 2),     
+            nn.ReLU(),                      
+            nn.MaxPool2d(2),                
+        )
+        # fully connected layer, output 10 classes
+        self.out = nn.Linear(32 * 7 * 7, 10)
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+        x = x.view(x.size(0), -1)       
+        output = self.out(x)
+        return output, x    # return x for visualization
+
+def train(cnn, loss_func, optimizer, train_data_loader, num_epochs):
+    # num_epochs = 10
+    cnn.train()
 
